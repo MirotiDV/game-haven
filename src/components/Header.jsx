62932c32a8726d935b8b2ce590239ddb07bd0d2b
Nyx Arturito <miguelroldanmart2001@outlook.es>
2025-10-31 12:23:14 +0100

    finished header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import { Fragment } from "react";
 import storeLogo from "../public/gamepad1.svg";
+import userAvatar from "../public/user-avatar.svg";
 
 export function Header() {
   return (
@@ -13,6 +13,31 @@ export function Header() {
         <a href="">Novedades</a>
         <a href="">Soporte</a>
       </nav>
+
+        <form role="search">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-search"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+
+          <input
+            required
+            type="text"
+            placeholder="Search games..."
+          />
+        </form>
 
       <button className="favButton">
         <svg
@@ -47,6 +72,9 @@ export function Header() {
           />
         </svg>
       </button>
+
+      <img className="avatarUser" src={userAvatar}></img>
+      
     </header>
   );
 }
