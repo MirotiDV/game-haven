1d2740c665a31b59963ee0077e8c8fcabd5820c3
MirotiDV <miguelroldanmart2001@outlook.es>
2025-11-19 19:28:07 +0100

    added new banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Header } from './components/Header.jsx'
-import { GameCard } from './components/GameCard.jsx'
 import { Banner } from './components/Banner.jsx'
 import './App.css'
 import { GameListing } from './components/GameListing.jsx';
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 export function Banner() {
   return (
     <section className="banner">
-      <img className="bannerImage" src="../src/public/banner.jpg" alt="Banner Image" />
+      <img className="bannerImage" src="../src/public/banner.png" alt="Banner Image" />
     </section>
   );
 }
